93e33de6f69f3bafbc2cb9a8d0f76dfdea20462d
Hoon Wee <mrgravity817@gmail.com>
2023-07-04 18:14:00 +0900

    docs: revise readme and template.ex

--- README.md
+++ README.md
@@ -3,17 +3,6 @@
 A quiz generator.  
 It generates quiz with predefined template.
 
-## Data structures for each nouns
+## Do fun things with loud worker-bees
 
-### Template
-
-- name: atom
-- category: atom
-- instructions: string
-
-### Question
-
-- raw: string
-- compiled: macro
-- generators: `%{substitution: list or function}`
-- checker: `function(substitution, string) -> boolean`
+Data / Functional Core / Tests / Boundaries / Lifecycle / Workers
--- lib/mastery/core/template.ex
+++ lib/mastery/core/template.ex
@@ -1,6 +1,16 @@
 defmodule Mastery.Core.Template do
+  @moduledoc ~S"""
+  - name(atom): the name of the template
+  - category(atom): a grouping for questions of the same name
+  - instructions(string): description that tells user how to answer the question.
+  - raw(string): the template code before compilation
+  - compiled(macro): the compiled version of the template code.
+  - generators(map of list/function): list of function that can be used to 
+                                      generate the values in template blanks.
+  - checker(function): checks if the answer is correct.
+  """
+  defstruct ~w[name category instructions raw compiled generators checker]a
   # ~w sigil creates list of strings.
   # with `a` prefix, it will instead create list of atoms.
-  defstruct ~w[name category instructions raw compiled generators checker]a
 end
 
