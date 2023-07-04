5da8349c963831fa39107dfd708364d5f277398a
Hoon Wee <mrgravity817@gmail.com>
2023-07-05 00:17:40 +0900

    feat: can create new quizzes with helper functions

--- lib/mastery/core/quiz.ex
+++ lib/mastery/core/quiz.ex
@@ -6,11 +6,12 @@ defmodule Mastery.Core.Quiz do
   - current_question(Question.t): the current question being 
     presented to the user.
   - last_reseponse(Response.t): the last response given by user.
-  - templates(map): the master list of templates by category.
+  - templates(%{cateogory => template[]}): the master list of templates by category.
   - used(list of Template.t): the templates that have benn mastered.
   - record(map of tempate=>integer): the number of correct 
      answers in a row for each template.
   """
+  alias Mastery.Core.Question
   alias Mastery.Core.Quiz
   alias Mastery.Core.Template
 
@@ -55,4 +56,80 @@ defmodule Mastery.Core.Quiz do
 
   defp add_to_list_or_nil(nil, template), do: [template]
   defp add_to_list_or_nil(templates, template), do: [template | templates]
+
+  def select_question(%__MODULE__{templates: t}) when map_size(t) == 0, do: nil
+
+  def select_question(quiz) do
+    quiz
+    |> pick_current_question
+    |> move_template(:used)
+    |> reset_template_cycle
+  end
+
+  defp pick_current_question(quiz) do
+    Map.put(quiz, :current_question, select_a_random_question(quiz))
+  end
+
+  def select_a_random_question(quiz) do
+    quiz.templates
+    |> Enum.random()
+    |> elem(1)
+    |> Enum.random()
+    |> Question.new()
+  end
+
+  defp move_template(quiz, field) do
+    quiz
+    |> remove_template_from_category
+    |> add_template_to_field(field)
+  end
+
+  defp template(quiz), do: quiz.current_question.template
+
+  defp remove_template_from_category(quiz) do
+    template = template(quiz)
+
+    new_category_templates =
+      quiz.templates
+      |> Map.fetch!(template.category)
+      |> List.delete(template)
+
+    new_templates =
+      if new_category_templates == [] do
+        Map.delete(
+          quiz.templates,
+          template.category
+        )
+      else
+        Map.put(
+          quiz.templates,
+          template.category,
+          new_category_templates
+        )
+      end
+
+    Map.put(quiz, :templates, new_templates)
+  end
+
+  defp add_template_to_field(quiz, field) do
+    template = template(quiz)
+    field_templates = Map.get(quiz, field)
+
+    Map.put(quiz, field, [template | field_templates])
+  end
+
+  defp reset_template_cycle(%{templates: templates, used: used} = quiz)
+       when map_size(templates) == 0 do
+    %__MODULE__{
+      quiz
+      | templates:
+          Enum.group_by(
+            used,
+            fn template -> template.category end
+          ),
+        used: []
+    }
+  end
+
+  defp reset_template_cycle(quiz), do: quiz
 end
