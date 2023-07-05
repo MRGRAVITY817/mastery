5fa1197ad71ceafdea8991da14745dacc36f80fa
Hoon Wee <mrgravity817@gmail.com>
2023-07-05 19:29:57 +0900

    feat: add quiz builder with two templates

--- test/support/quiz_builders.exs
+++ test/support/quiz_builders.exs
@@ -67,4 +67,10 @@ defmodule QuizBuilders do
     |> Template.new()
     |> Question.new()
   end
+
+  def build_quiz_with_two_templates(quiz_overrides \\ []) do
+    build_quiz(quiz_overrides)
+    |> Quiz.add_template(template_fields())
+    |> Quiz.add_template(double_digit_addition_template_fields())
+  end
 end
