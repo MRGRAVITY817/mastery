e252d035db5934ce0f0c7c68426d896b30bb4ec7
Hoon Wee <mrgravity817@gmail.com>
2023-07-07 07:58:31 +0900

    feat: add service api to hide inner machinery

--- lib/mastery/boundary/quiz_manager.ex
+++ lib/mastery/boundary/quiz_manager.ex
@@ -27,4 +27,18 @@ defmodule Mastery.Boundary.QuizManager do
   def handle_call({:lookup_quiz_by_title, quiz_title}, _from, quizzes) do
     {:reply, quizzes[quiz_title], quizzes}
   end
+
+  # Service APIs
+
+  def build_quiz(manager \\ __MODULE__, quiz_fields) do
+    GenServer.call(manager, {:build_quiz, quiz_fields})
+  end
+
+  def add_template(manager \\ __MODULE__, quiz_title, template_fields) do
+    GenServer.call(manager, {:add_template, quiz_title, template_fields})
+  end
+
+  def lookup_quiz_by_title(manager \\ __MODULE__, quiz_title) do
+    GenServer.call(manager, {:lookup_quiz_by_title, quiz_title})
+  end
 end
