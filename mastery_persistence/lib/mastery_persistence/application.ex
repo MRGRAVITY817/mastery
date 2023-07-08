f33bb588a8d7b0e21f6a08987adac7060f712de0
Hoon Wee <mrgravity817@gmail.com>
2023-07-09 08:14:32 +0900

    feat: add postgres ecto setup

--- mastery_persistence/lib/mastery_persistence/application.ex
+++ mastery_persistence/lib/mastery_persistence/application.ex
@@ -1,19 +1,18 @@
 defmodule MasteryPersistence.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
   @moduledoc false
 
   use Application
 
+  use Ecto.Repo,
+    otp_app: :mastery_persistence,
+    adapter: Ecto.Adapters.Postgres
+
   @impl true
   def start(_type, _args) do
     children = [
-      # Starts a worker by calling: MasteryPersistence.Worker.start_link(arg)
-      # {MasteryPersistence.Worker, arg}
+      MasteryPersistence.Repo
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: MasteryPersistence.Supervisor]
     Supervisor.start_link(children, opts)
   end
