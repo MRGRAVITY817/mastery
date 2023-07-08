fcd94f2ee862bac0a23ec25f5322e8b65d507bf0
Hoon Wee <mrgravity817@gmail.com>
2023-07-09 08:52:45 +0900

    feat: record response coming from core

--- mastery_persistence/lib/mastery_persistence.ex
+++ mastery_persistence/lib/mastery_persistence.ex
@@ -2,17 +2,28 @@ defmodule MasteryPersistence do
   @moduledoc """
   Documentation for `MasteryPersistence`.
   """
+  import Ecto.Query, only: [from: 2]
+  alias MasteryPersistence.{Repo, Response}
 
-  @doc """
-  Hello world.
+  def record_response(response, in_transaction \\ fn _response -> :ok end) do
+    {:ok, result} =
+      Repo.transaction(fn ->
+        %{
+          quiz_title: to_string(response.quiz_title),
+          template_name: to_string(response.template_name),
+          to: response.to,
+          email: response.email,
+          answer: response.correct,
+          correct: response.correct,
+          inserted_at: response.timestamp,
+          updated_at: response.timestamp
+        }
+        |> Response.record_changeset()
+        |> Repo.insert!()
 
-  ## Examples
+        in_transaction.(response)
+      end)
 
-      iex> MasteryPersistence.hello()
-      :world
-
-  """
-  def hello do
-    :world
+    result
   end
 end
