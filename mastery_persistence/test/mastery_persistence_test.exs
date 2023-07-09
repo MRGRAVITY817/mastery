93c0a89a1591be93a703c5e89ae038f74d6dc040
Hoon Wee <mrgravity817@gmail.com>
2023-07-09 15:43:00 +0900

    feat: add test suite setup for mastery_persistence

--- mastery_persistence/test/mastery_persistence_test.exs
+++ mastery_persistence/test/mastery_persistence_test.exs
@@ -1,8 +1,22 @@
 defmodule MasteryPersistenceTest do
   use ExUnit.Case
-  doctest MasteryPersistence
+  alias MasteryPersistence.{Response, Repo}
 
-  test "greets the world" do
-    assert MasteryPersistence.hello() == :world
+  describe "Mastery Persistence Layer" do
+    setup do
+      :ok = Ecto.Adapters.SQL.Sandbox.checkout(Repo)
+
+      response = %{
+        quiz_title: :simple_addition,
+        template_name: :single_digit_addition,
+        to: "3 + 4",
+        email: "student@example.com",
+        answer: "7",
+        correct: true,
+        timestamp: DateTime.utc_now()
+      }
+
+      {:ok, %{response: response}}
+    end
   end
 end
